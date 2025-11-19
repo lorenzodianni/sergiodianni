a9d188467c91622b8e0544055b9da670858276f1
Lorenzo D'Ianni <lorenzo.dianni@qwentes.it>
2025-11-19 21:20:26 +0100

    fix: build SSG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,11 @@ import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessCh
 import {provideRouter, withInMemoryScrolling} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top'})),
-    provideClientHydration(withEventReplay()),
   ]
 };
