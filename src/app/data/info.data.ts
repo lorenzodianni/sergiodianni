42e6026e991f0198ee3d94368b34267ec3ff30d8
Lorenzo D'Ianni <lorenzo.dianni@qwentes.it>
2025-11-19 22:21:42 +0100

    feat: logo + email

--- src/app/data/info.data.ts
+++ src/app/data/info.data.ts
@@ -1,6 +1,30 @@
 export const CONTACT_INFO = {
   phone: '+39 02 1234567',
-  email: 'info@studio.it',
+  email: `
+<div class="email">
+<span>i</span>
+<span class="nsd">x</span>
+<span>n</span>
+<span class="nsd">x</span>
+<span>f</span>
+<span class="nsd">x</span>
+<span>o</span>
+<span class="nsd">x</span>
+<span>@</span>
+<span class="nsd">x</span>
+<span>s</span>
+<span class="nsd">x</span>
+<span>t</span>
+<span class="nsd">x</span>
+<span>u</span>
+<span class="nsd">x</span>
+<span>d</span>
+<span class="nsd">x</span>
+<span>i</span>
+<span class="nsd">x</span>
+<span>o</span>
+</div>
+  `,
   pec: 'studio@pec.it',
   address: {
     street: 'Viale delle Medaglie d\'Oro 176',
