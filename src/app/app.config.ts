2fc03eab1b5b9c58f47d0132ae29b78daef30ca1
Lorenzo D'Ianni <lorenzo.dianni@qwentes.it>
2025-11-23 13:54:38 +0100

    fix: anchor link

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top'})),
+    provideRouter(routes, withInMemoryScrolling({scrollPositionRestoration: 'top', anchorScrolling: 'enabled'})),
   ]
 };
